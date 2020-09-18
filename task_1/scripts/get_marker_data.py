3d9fb234452f2f52704c0e1f5cb473716542a4cd
Ashwin-Rajesh <aswinrajesh2@gmail.com>
2020-09-18 23:22:37 +0530

    Added task1

---
+++ task_1/scripts/get_marker_data.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import rospy
+import roslib
+import tf
+
+from geometry_msgs.msg import PoseArray
+
+#Define a class
+class Whycon_detect():
+
+	def __init__(self):
+		
+
+
+if __name__=="__main__":
+	marker = Whycon_detect()
+	while not rospy.is_shutdown():
+		rospy.spin()
