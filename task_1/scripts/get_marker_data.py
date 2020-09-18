a3c4910b32f1eb440333265148d53d1f7103c33d
Ashwin-Rajesh <aswinrajesh2@gmail.com>
2020-09-19 00:01:40 +0530

    Added tasks folder that stores pdfs

--- task_1/scripts/get_marker_data.py
+++ task_1/scripts/get_marker_data.py
@@ -5,13 +5,21 @@ import roslib
 import tf
 
 from geometry_msgs.msg import PoseArray
+from geometry_msgs.msg import Point
 
 #Define a class
 class Whycon_detect():
 
 	def __init__(self):
-		
-
+		rospy.init_node("whycon_detection")
+		self.whycon_coordinates = {}
+		self.sub = rospy.Subscriber("/whycon/poses", PoseArray, self.callback)
+	
+	def callback(self, msg):
+		for i,pos in enumerate(msg.poses):
+			p = pos.position
+			self.whycon_coordinates[i] = map(lambda x : round(x, 3), [p.x,p.y,p.z])
+		print(self.whycon_coordinates)
 
 if __name__=="__main__":
 	marker = Whycon_detect()
